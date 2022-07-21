9810d8fa059b02d3cca4eb4af1c2971560096e90
Nicholas Poet <npoet@allathlete.com>
2022-07-20 22:16:24 -0700

    print map if not empty

--- internal/pkg/utils.go
+++ internal/pkg/utils.go
@@ -25,26 +25,28 @@ func Check(e error) {
 }
 
 func PrintMap(worldmap map[string]*City) {
-	// PrintMap prints the final map in the format of the input text file
-	fmt.Println("Final map output:")
-	keys := map[string]bool{}
-	for i := range worldmap {
-		keys[worldmap[i].Name] = true
-	}
-	for j := range worldmap {
-		var outputString []string
-		outputString = append(outputString, worldmap[j].Name)
-		initialFields := strings.Fields(worldmap[j].Initial)
-		for k := range initialFields {
-			s := strings.Split(initialFields[k], "=")
-			if keys[s[1]] {
-				outputString = append(outputString, initialFields[k])
+	// PrintMap prints the final map in the format of the input text file, if any cities remain
+	if len(worldmap) > 0 {
+		fmt.Println("Final map output:")
+		keys := map[string]bool{}
+		for i := range worldmap {
+			keys[worldmap[i].Name] = true
+		}
+		for j := range worldmap {
+			var outputString []string
+			outputString = append(outputString, worldmap[j].Name)
+			initialFields := strings.Fields(worldmap[j].Initial)
+			for k := range initialFields {
+				s := strings.Split(initialFields[k], "=")
+				if keys[s[1]] {
+					outputString = append(outputString, initialFields[k])
+				}
 			}
+			for l := range outputString {
+				fmt.Print(outputString[l] + " ")
+			}
+			fmt.Println()
 		}
-		for l := range outputString {
-			fmt.Print(outputString[l] + " ")
-		}
-		fmt.Println()
 	}
 }
 
