6ba495217d3263c2eda4b9d9ee1b033ecfb61137
Nicholas Poet <npoet@allathlete.com>
2022-07-20 22:34:39 -0700

    add tests

--- internal/pkg/utils_test.go
+++ internal/pkg/utils_test.go
@@ -2,9 +2,8 @@ package pkg
 
 import "testing"
 
-func TestPrintMap(t *testing.T) {}
-
 func TestReverseMap(t *testing.T) {
+	// TestReverseMap checks that directions are properly reversed and output structure retained
 	initial := "Bar"
 	directions := map[string]string{"north": "south", "south": "north", "east": "west", "west": "east"}
 	for i := range directions {
